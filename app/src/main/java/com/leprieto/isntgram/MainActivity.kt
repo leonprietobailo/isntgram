bdd377eddac3227619252e0567bf7048577f0817
León E. Prieto Bailo <leonprietobailo@gmail.com>
2024-11-23 19:45:30 +0100

    Feature: Implement initial user profile UI components.

--- app/src/main/java/com/leprieto/isntgram/MainActivity.kt
+++ app/src/main/java/com/leprieto/isntgram/MainActivity.kt
@@ -1,27 +1,55 @@
 package com.leprieto.isntgram
 
+//import androidx.compose.foundation.layout.FlowRowScopeInstance.align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells.Fixed
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 import com.leprieto.isntgram.ui.theme.IsntGramTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +59,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             IsntGramTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                    Greeting(
+//                        name = "Android", modifier = Modifier.padding(innerPadding)
+//                    )
+                    MainComposable(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -44,61 +72,236 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
+////@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    IsntGramTheme {
+//        Greeting("Android")
+//    }
+//}
+
+
 @Composable
-fun GreetingPreview() {
-    IsntGramTheme {
-        Greeting("Android")
+@Preview(showBackground = true)
+fun MainComposable(modifier: Modifier = Modifier.padding(12.dp)) {
+    Column(modifier = modifier) {
+        ProfileTopBar()
+        Body()
     }
 }
 
-@Preview(showSystemUi = true, showBackground = true)
+
+@Composable
+fun Body() {
+    Column(modifier = Modifier.padding(12.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding()
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(CircleShape)
+                    .border(BorderStroke(2.dp, Color.Gray), CircleShape),
+                painter = painterResource(id = R.drawable.ic_person),
+                contentDescription = "Profile Picture",
+
+                )
+            Row(
+                modifier = Modifier
+                    .weight(3f)
+                    .padding(20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                ProfileNumberIndicator(2, "posts")
+                ProfileNumberIndicator(1143, "followers")
+                ProfileNumberIndicator(727, "following")
+            }
+        }
+        Text(
+            "León Prieto", fontSize = 12.sp, modifier = Modifier.padding(
+                top = 12.dp
+            ), fontWeight = FontWeight.Bold
+        )
+        Text(
+            stringResource(R.string.lorem_ipsum), fontSize = 12.sp
+
+        )
+
+        Row(modifier = Modifier.padding(vertical = 12.dp)) {
+            FilledTonalButton(
+                modifier = Modifier.weight(4f),
+                shape = RoundedCornerShape(12.dp),
+                onClick = {}
+            ) {
+                Text("Edit profile")
+            }
+            Spacer(modifier = Modifier.size(8.dp))
+            FilledTonalButton(
+                modifier = Modifier.weight(4f),
+                shape = RoundedCornerShape(12.dp),
+                onClick = {}
+            ) {
+                Text("Edit profile")
+            }
+            Spacer(modifier = Modifier.size(8.dp))
+            FilledTonalButton(
+                modifier = Modifier.weight(1f),
+                shape = RoundedCornerShape(12.dp),
+                contentPadding = PaddingValues(0.dp),
+                onClick = {}
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_add_person),
+                    contentDescription = "Add image",
+                    modifier = Modifier.size(20.dp)
+                )
+            }
+        }
+
+        var selectedTab by remember { mutableIntStateOf(0) }
+
+        TabRow(
+            selectedTabIndex = selectedTab,
+
+            ) {
+            Tab(
+                selected = selectedTab == 0,
+                onClick = { selectedTab = 0 },
+                content = {
+                    Box(modifier = Modifier.padding(16.dp)) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_dashboard),
+                            contentDescription = "",
+                            modifier = Modifier.size(20.dp)
+                        )
+                    }
+                }
+            )
+            Tab(
+                selected = selectedTab == 1,
+                onClick = { selectedTab = 1 },
+                content = {
+                    Box(modifier = Modifier.padding(16.dp)) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_reels),
+                            contentDescription = "",
+                            modifier = Modifier.size(20.dp)
+                        )
+                    }
+                }
+            )
+            Tab(
+                selected = selectedTab == 2,
+                onClick = { selectedTab = 2 },
+                content = {
+                    Box(modifier = Modifier.padding(16.dp)) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_tagged),
+                            contentDescription = "",
+                            modifier = Modifier.size(20.dp)
+                        )
+                    }
+                }
+            )
+        }
+
+        when (selectedTab) {
+            0 -> Dashboard()
+            1 -> Reels()
+            2 -> Tagged()
+        }
+    }
+}
+
+@Composable
+fun Dashboard() {
+    LazyVerticalGrid(
+        columns = Fixed(3),
+        horizontalArrangement = Arrangement.spacedBy(2.dp),
+        verticalArrangement = Arrangement.spacedBy(2.dp),
+    ) {
+        items(700) {
+            Box(modifier = Modifier.aspectRatio(1f)) {
+                val picNumber = it + 1
+                AsyncImage(
+                    model = "https://yavuzceliker.github.io/sample-images/image-$picNumber.jpg",
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Reels() {
+    Box() {
+        Text("ToDO")
+    }
+}
+
+@Composable
+fun Tagged() {
+    Box() {
+        Text("ToDO")
+    }
+}
+
+@Composable
+fun ProfileNumberIndicator(number: Int, text: String) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(number.toString(), fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Text(text)
+    }
+}
+
+
+//@Preview(showSystemUi = true, showBackground = true)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainBox() {
-    TopAppBar(
-        title = {  }, modifier = Modifier.padding(12.dp),
-        actions = {
-//            Box(modifier = Modifier.size(25.dp).background(Color.Blue))
-            Icon(
-                painter = painterResource(R.drawable.ic_lock),
-                contentDescription = "Add image",
-                modifier = Modifier.size(40.dp),
-                tint = Color.Black
-            )
-            Text("omegaisugly")
-            Spacer(modifier = Modifier.weight(1f))
-            Icon(
-                painter = painterResource(R.drawable.ic_threads),
-                contentDescription = "Add image",
-                modifier = Modifier.size(40.dp),
-                tint = Color.Black
-            )
+fun ProfileTopBar() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 12.dp)
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_lock),
+            contentDescription = "Add image",
+            modifier = Modifier.size(16.dp),
+            tint = Color.Black
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text("omegaisugly", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+        Spacer(modifier = Modifier.weight(1f))
+        Icon(
+            painter = painterResource(R.drawable.ic_threads),
+            contentDescription = "Add image",
+            modifier = Modifier.size(32.dp),
+            tint = Color.Black
+        )
 
-            Spacer(modifier = Modifier.width(16.dp))
+        Spacer(modifier = Modifier.width(16.dp))
 
-            Icon(
-                painter = painterResource(R.drawable.ic_action_name),
-                contentDescription = "Add image",
-                modifier = Modifier.size(40.dp),
-                tint = Color.Black
-            )
-
-            Spacer(modifier = Modifier.width(16.dp))
-            Icon(
-                imageVector = Icons.Default.Menu,
-                contentDescription = "Default Burger Menu",
-                modifier = Modifier.size(40.dp)
-            )
-
-        }
-    )
-
-
+        Icon(
+            painter = painterResource(R.drawable.ic_action_name),
+            contentDescription = "Add image",
+            modifier = Modifier
+                .size(32.dp)
+                .alpha(100f),
+            tint = Color.Black
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Icon(
+            imageVector = Icons.Default.Menu,
+            contentDescription = "Default Burger Menu",
+            modifier = Modifier.size(32.dp)
+        )
+    }
 }
 
